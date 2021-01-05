a6fecfd9cf9615373dab32694361d66715e22703
github username <github email address>
2021-01-05 16:11:45 -0500

    hurdles

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -12,19 +12,29 @@ class Bathroom < ActiveRecord::Base
   end
 
   def self.best_flush
-    Review.all.select{|level| level.flush_factor == "jet engine"}
+    Review.all.select{|flush| flush.flush_factor == "jet engine"}
   end
   
   def self.short_wait
-    Review.all.select{|level| level.wait_time < 6}
+    Review.all.select{|wait| wait.wait_time < 6}
   end
 
   def self.cleanest
-    Review.all.select{|level| level.cleanliness > 8}
+    Review.all.select{|clean| clean.cleanliness > 8}
+  end
+
+  def self.handicap_access
+    Review.all.select{|access| access.handicap_accessible == true}
+  end
+
+  def self.baby_station
+    Review.all.select{|baby| baby.baby_changing_station == true}
   end
 
   def self.most_reviews
     Bathroom.all.sort_by{|bathroom| bathroom.reviews.length}.last(2)
   end 
+
+    #create new review, which creates new bathroom as well
 
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -6,4 +6,31 @@ class Neighborhood < ActiveRecord::Base
     Bathroom.all.select{|bathroom| bathroom.neighborhood == self}
   end
 
+  def most_popular_in_neighborhood
+    self.all_bathrooms.sort_by{|bathroom| bathroom.reviews.length}.last(2)
+  end
+
+  def low_security
+    self.all_bathrooms.select{|level| Review.find_by(level.security_level) == "low"}
+  end
+
+  def best_flush
+    self.all_bathrooms.select{|flush| flush.flush_factor == "jet engine"}
+  end
+  
+  def short_wait
+    self.all_bathrooms.select{|wait| wait.wait_time < 6}
+  end
+
+  def cleanest
+    self.all_bathrooms.select{|clean| clean.cleanliness > 8}
+  end
+
+  def handicap_access
+    self.all_bathrooms.select{|access| access.handicap_accessible == true}
+  end
+
+  def baby_station
+    self.all_bathrooms.select{|baby| baby.baby_changing_station == true}
+  end
 end
